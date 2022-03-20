ce8f2691dfe02540bedb3653eaa1bef7193146cb
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-20 23:35:53 +0700

    feat: get all job api + serializing data

--- job/views.py
+++ job/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import JobSerializer
+from .models import Job
 
 # Create your views here.
+@api_view(['GET'])
+def getAllJobs(request):
+    
+    jobs = Job.objects.all()
+    
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
