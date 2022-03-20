c5f7c36b4b27c0d78c0654adfe74925b660a2063
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-20 23:13:22 +0700

    feat: migration

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis'
+    'django.contrib.gis',
+    'rest_framework',
+    'corsheaders',
+    # 'storages',
+    'django_filters',
+    
+    'job.apps.JobConfig'
 ]
 
 MIDDLEWARE = [
