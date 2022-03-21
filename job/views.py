925cc3b4f093d42c2cb8f659af5203d60f6e275f
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-21 21:02:27 +0700

    feat: get stats topic

--- job/views.py
+++ job/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Avg, Min, Max, Count
 
 from .serializers import JobSerializer
 from .models import Job
@@ -57,3 +58,21 @@ def deleteJob(request, pk):
     job.delete()
     
     return Response({ 'message': 'Job is deleted.' }, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def getTopicStats(request, topic):
+    args = { 'title__icontains': topic }
+    jobs = Job.objects.filter(**args)
+    
+    if len(jobs) == 0:
+        return Response({ 'message': 'Not stats found for {topic}'.format(topic=topic) })
+    
+    stats = jobs.aggregate(
+        total_jobs = Count('title'),
+        avg_position = Avg('position'),
+        avg_salary = Avg('salary'),
+        min_salary = Min('salary'),
+        max_salary = Max('salary')
+    )
+    
+    return Response(stats)
