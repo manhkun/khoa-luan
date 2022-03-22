a317355f2e3e827947823bdf96f8c8a5baec8906
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-22 20:17:52 +0700

    feat: auth app + jwt config

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import os
 import dotenv
@@ -47,7 +48,8 @@ INSTALLED_APPS = [
     # 'storages',
     'django_filters',
     
-    'job.apps.JobConfig'
+    'job.apps.JobConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -113,6 +115,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
+    'AUTH_HEADER_TYPES': ('Bearer', ),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.token.AccessToken'),
+}
 
 
 # Internationalization
