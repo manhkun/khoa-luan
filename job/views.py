b46536d045fb6312a9fc07efcc74945d34861071
Quốc Mạnh Phùng <kunpro1st@gmail.com>
2022-03-26 22:43:19 +0700

    fix: get job with candidate

--- job/views.py
+++ job/views.py
@@ -38,9 +38,11 @@ def getAllJobs(request):
 @api_view(['GET'])
 def getJob(request, pk):
     job = get_object_or_404(Job, id=pk)
+    candidates = job.candidatesapplied_set.all().count()
+
     serializer = JobSerializer(job, many=False)
     
-    return Response(serializer.data)
+    return Response({ 'job': serializer.data, 'candidates': candidates })
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
